a97dc55e594249b132b7139074f42a0a3ee81a77
Ammo <ammo@slne.dev>
2025-05-13 16:35:42 +0200

    chore: Update dependencies and comment out unused libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ surfPaperPluginApi {
     generateLibraryLoader(false)
 
     serverDependencies {
-        registerRequired("PlaceholderAPI")
+//        registerRequired("Nexo")
     }
 
     runServer {
@@ -21,30 +21,10 @@ surfPaperPluginApi {
 }
 
 dependencies {
-    compileOnlyApi(libs.paper.api)
-    compileOnlyApi(libs.commandapi)
-    compileOnlyApi(libs.placeholder.api)
-    compileOnlyApi(libs.oraxen)
-    compileOnlyApi(libs.betterhud.standard)
-    compileOnlyApi(libs.betterhud.bukkit)
-    compileOnlyApi(libs.betterhud.commands)
+    compileOnlyApi(libs.nexo)
     compileOnlyApi(libs.chestprotect)
-
-    api(libs.inventory.framework)
-    api(libs.configurate.core)
-    api(libs.configurate.yml)
 }
 
 group = "de.castcrafter"
 version = "2.0.0-SNAPSHOT"
 description = "loot_drop"
-
-tasks {
-    shadowJar {
-        relocate(
-            "com.github.stefvanschie.inventoryframework",
-            "de.castcrafter.lootdrop.inventoryframework"
-        )
-        relocate("org.spongepowered", "de.castcrafter.lootdrop.spongepowered")
-    }
-}
