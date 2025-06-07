c3057830767546edec9013181fbdae06a0d2ad8d
Keviro <25409956+Keviro@users.noreply.github.com>
2025-06-07 11:03:00 +0200

    feat: add respawn listener to teleport players to The End

--- src/main/kotlin/de/castcrafter/lootdrop/listener/ListenerManager.kt
+++ src/main/kotlin/de/castcrafter/lootdrop/listener/ListenerManager.kt
@@ -3,6 +3,7 @@ package de.castcrafter.lootdrop.listener
 import de.castcrafter.lootdrop.listener.listeners.ChestChestProtectListener
 import de.castcrafter.lootdrop.listener.listeners.ChestListener
 import de.castcrafter.lootdrop.listener.listeners.JoinListener
+import de.castcrafter.lootdrop.listener.listeners.RespawnListener
 import de.castcrafter.lootdrop.listener.listeners.SeamineListener
 import de.castcrafter.lootdrop.listener.listeners.SpecialItemListener
 import de.castcrafter.lootdrop.locator.LocatorListener
@@ -14,6 +15,7 @@ object ListenerManager {
     fun registerListeners() {
         SeamineListener.register()
         JoinListener.register()
+        RespawnListener.register()
 
         ChestListener.register()
         if (pluginManager.isPluginEnabled("ChestProtect")) {
