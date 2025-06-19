5fc94872e2c22e47cdad3f60fa324b468f1d4e25
Keviro <25409956+Keviro@users.noreply.github.com>
2025-06-19 22:38:15 +0200

    remove only_end event stuff

--- src/main/kotlin/de/castcrafter/lootdrop/listener/ListenerManager.kt
+++ src/main/kotlin/de/castcrafter/lootdrop/listener/ListenerManager.kt
@@ -2,8 +2,6 @@ package de.castcrafter.lootdrop.listener
 
 import de.castcrafter.lootdrop.listener.listeners.ChestChestProtectListener
 import de.castcrafter.lootdrop.listener.listeners.ChestListener
-import de.castcrafter.lootdrop.listener.listeners.JoinListener
-import de.castcrafter.lootdrop.listener.listeners.RespawnListener
 import de.castcrafter.lootdrop.listener.listeners.SeamineListener
 import de.castcrafter.lootdrop.listener.listeners.SpecialItemListener
 import de.castcrafter.lootdrop.locator.LocatorListener
@@ -14,8 +12,6 @@ object ListenerManager {
 
     fun registerListeners() {
         SeamineListener.register()
-        JoinListener.register()
-        RespawnListener.register()
 
         ChestListener.register()
         if (pluginManager.isPluginEnabled("ChestProtect")) {
